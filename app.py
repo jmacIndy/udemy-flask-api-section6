ea700c9aa3c4d7b6091f3d1eb912cc5cd064b6c8
Jeff McGinley <jeff.mcginley.indy@gmail.com>
2018-10-06 06:52:46 -0400

    let SQLAlchemy create the tables

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jeff'      # should be more secure
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+
+    db.create_all()
+
 jwt = JWT(app, authenticate, identity)    # creates new endpoint /auth
 
 api.add_resource(Item, '/item/<string:name>')
